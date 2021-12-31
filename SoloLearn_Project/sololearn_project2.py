b63209888d9d31dc7772331936fcc1f963d3915f
Yohannes Assebe <39563000+JohnAssebe@users.noreply.github.com>
2021-12-31 22:22:07 +0300

    Update sololearn_project2.py

--- SoloLearn_Project/sololearn_project2.py
+++ SoloLearn_Project/sololearn_project2.py
@@ -1,3 +1,7 @@
+'''
+@author Yohannes Assebe
+sololearn python data science course project 2 question answer 
+'''
 import numpy as np
 data = np.array([150000, 125000, 320000, 540000, 200000, 120000, 160000, 230000, 280000, 290000, 300000, 500000, 420000, 100000, 150000, 280000])
 mean_value=np.mean(data)
