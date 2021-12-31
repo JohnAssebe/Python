1685eea392405430d8ff1eeec93dcf1fb8886f2b
Yohannes Assebe <39563000+JohnAssebe@users.noreply.github.com>
2021-12-31 22:20:18 +0300

    deviation from mean percentage

---
+++ SoloLearn_Project/sololearn_project2.py
@@ -0,0 +1,10 @@
+import numpy as np
+data = np.array([150000, 125000, 320000, 540000, 200000, 120000, 160000, 230000, 280000, 290000, 300000, 500000, 420000, 100000, 150000, 280000])
+mean_value=np.mean(data)
+std_data=np.std(data)
+total_n=data.size
+counter=0
+for i in range(total_n):
+    if mean_value-std_data<=data[i]<=mean_value+std_data:
+        counter+=1
+print((counter/total_n)*100)
