b5b36001c357844dd93b2e2d093ccce2f7cf9707
Yohannes Assebe <39563000+JohnAssebe@users.noreply.github.com>
2022-02-23 20:37:11 +0300

    sock match problem

---
+++ HackerRank/sales_by_match.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    set_arr=set(ar)
+    sock_counter=0
+    for i in set_arr:
+        if ar.count(i)>=2:
+            sock_counter+=ar.count(i)//2
+    return sock_counter
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
