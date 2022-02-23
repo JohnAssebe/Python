5c7394a47e913364d00b440b51a1a8cb5b1e972d
Yohannes Assebe <39563000+JohnAssebe@users.noreply.github.com>
2022-02-23 20:24:22 +0300

    Bill Division

---
+++ HackerRank/bill_division.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    bill.pop(k)
+    bill_of_ann=(sum(bill))//2
+    if bill_of_ann==b:
+        return 'Bon Appetit'
+    else:
+        return b-bill_of_ann
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    print(bonAppetit(bill, k, b))
